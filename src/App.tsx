bd4e150a1dcd3c4ac95ea1e8228d20c2238d2bc8
HatyushinAS <kakdevonte@yandex.ru>
2022-08-10 10:35:53 +0300

    add Planet component and PlanetsPage

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { PlanetsPage } from './pages/PlanetsPage';
 export const App: React.FC = () => {
   return (
     <div className="wrapper">
-      <h1>Star Wars</h1>
+      <h1 className="title">Star Wars</h1>
       <Routes>
         <Route path={'/'} element={<PlanetsPage />} />
       </Routes>
