eb2159bf2121bd4626ca600d8fd3d353ed8b7399
HatyushinAS <kakdevonte@yandex.ru>
2022-08-10 07:40:07 +0300

    add Planet component and PlanetsPage

--- src/components/Planet/index.tsx
+++ src/components/Planet/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import styles from './Planet.module.scss';
 
 export const Planet: React.FC = () => {
-    return (
-        <div className={styles.root}>
-            <div className={styles.icon}></div>
-            <div className={styles.info}>
-                <h2>Name - {'Tatooine'}</h2>
-                <span>Population - {'200000'}</span>
-                <span>Terrain - {'desert'}</span>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.root}>
+      <div className={styles.icon}></div>
+      <div className={styles.info}>
+        <h2>{'Tatooine'}</h2>
+        <span>Population: {'200000'}</span>
+        <span>Terrain: {'desert'}</span>
+      </div>
+    </div>
+  );
 };
