1d51bcf03cb97a3fa01e087410eb92f671d00676
HatyushinAS <kakdevonte@yandex.ru>
2022-08-11 11:39:10 +0300

    add Infinite Scroll in PlanetsPage

--- src/pages/PlanetsPage/index.tsx
+++ src/pages/PlanetsPage/index.tsx
@@ -4,42 +4,29 @@ import styles from './PlanetsPage.module.scss';
 import { useAppDispatch, useAppSelector } from '../../redux/store';
 import { getPlanets } from '../../redux/planets/asyncActions';
 
-const options = {
-  rootMargin: '0px',
-  threshold: 0.5,
-};
-
 export const PlanetsPage: React.FC = () => {
   const dispatch = useAppDispatch();
   const { results, next } = useAppSelector((state) => state.planets);
   const [page, setPage] = React.useState(1);
   const [isFetching, setIsFetching] = React.useState(false);
-  const observer = React.useRef();
-  const lastPlanetElementRef = React.useRef<HTMLDivElement>(null);
+  const observer = React.useRef<IntersectionObserver>();
 
-  const callbackFunc = (entries: any) => {
-    console.log('.asdsa');
-    const [entry] = entries;
-    setPage(page + 1);
-    dispatch(getPlanets(page + 1));
-  };
+  const lastPlanetElementRef = React.useCallback(
+    (node: HTMLDivElement) => {
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && next) {
+          setPage((page) => page + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [next]
+  );
 
   React.useEffect(() => {
     dispatch(getPlanets(page));
-  }, []);
-
-  React.useEffect(() => {
-    console.log('Меню обсервер');
-    const observer = new IntersectionObserver(callbackFunc, options);
-
-    if (lastPlanetElementRef.current)
-      observer.observe(lastPlanetElementRef.current);
-
-    return () => {
-      if (lastPlanetElementRef.current)
-        observer.unobserve(lastPlanetElementRef.current);
-    };
-  }, [lastPlanetElementRef]);
+  }, [page]);
 
   return (
     <div className={styles.root}>
